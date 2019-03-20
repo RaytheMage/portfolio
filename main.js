85b960360d3d4efae832c6cf6f226dce4059a238
Rachel Rasmussen <rachel.rasmussen.511@gmail.com>
2019-03-19 22:33:10 -0600

    add js file

---
+++ main.js
@@ -0,0 +1,11 @@
+import { films } from './films.js'
+import { people } from './people.js'
+import { planets } from './planets.js'
+import { species } from './species.js'
+import { starship } from './starships.js'
+import { vehicles } from './vehicles.js'
+
+films. forEach((film)=> {
+    console.log(film.title)
+    
+})
